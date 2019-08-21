8f40368447adedf5269fad3b61c29e6257f2a095
muhammad idrees <idris@y7mail.com>
2019-08-22 00:38:08 +0930

    Updates

---
+++ precache-manifest.74c396694b5f2989e267aefaf99f451f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc1a2abbf2387e0882416e4f17d19aa",
+    "url": "/RSVP-APP/index.html"
+  },
+  {
+    "revision": "20fe6300c1be8b5c936b",
+    "url": "/RSVP-APP/static/css/main.754ee8f6.chunk.css"
+  },
+  {
+    "revision": "94e4908063a116f9d528",
+    "url": "/RSVP-APP/static/js/2.4351708f.chunk.js"
+  },
+  {
+    "revision": "20fe6300c1be8b5c936b",
+    "url": "/RSVP-APP/static/js/main.7f58088b.chunk.js"
+  },
+  {
+    "revision": "833674e8a97a926de02b",
+    "url": "/RSVP-APP/static/js/runtime~main.413aa78c.js"
+  }
+]);
